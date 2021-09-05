05b27ae5608ea6e0df20175ffa54ce7f49a7f20d
danieski <53768766+danieski@users.noreply.github.com>
2021-09-05 13:11:38 +0200

    Add files via upload

---
+++ codificador de palabras.py
@@ -0,0 +1,18 @@
+
+# Queremos codificar una palabra
+def codificador(frase):
+    encriptada = ""
+    for letra in frase:
+        if letra in "aeiou":
+            if letra is "AEIOU":
+                encriptada = encriptada + "Z"
+            else:
+                encriptada = encriptada + "z"
+        else :
+            encriptada = encriptada + letra
+    return encriptada
+
+
+
+print(codificador(input("Ponga fascismo: ")))
+
