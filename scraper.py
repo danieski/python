cd7ffb7df9d65dbe79f9360b1acb6d84cd37c0ab
danieski <53768766+danieski@users.noreply.github.com>
2022-04-05 11:20:27 +0100

    Update scraper.py
    Find out how to get the following pages from there.

--- scraper.py
+++ scraper.py
@@ -1,25 +1,19 @@
-from unittest import result
 import requests
 from bs4 import BeautifulSoup
+for a in range(1,2):
 
+    url='https://www.goldenpages.ie/q/business/advanced/what/credit%20union/'
+    result = requests.get(url)
+    content = result.text
+    soup = BeautifulSoup(content, 'lxml')
+    container = soup.find_all('div', class_='listing_container')
+    phone = soup.find_all('a', class_='link_listing_number')
 
-
-url='https://www.goldenpages.ie/q/business/advanced/what/credit%20union/'
-
-result = requests.get(url)
-content = result.text
-
-soup = BeautifulSoup(content, 'lxml')
-
-for result in soup.find_all('a', class_="listing_title_link"):
-	result.get_text()
-	mediaurl = result.get('href')
-	enteraurl = "https://www.goldenpages.ie" + mediaurl
-    result = requests.get(enteraurl)
-
-
-#box = soup.find_all('div', class_='listing_content')
-#title = box('a', class_= "listing_title_link").get_text()
-#phone = box('a', class_="link_listing_number").get_text()
-#print(phone)
-#print("Phone number:%s" %phone)
+    for i in container:
+	    title = i.find('a', class_='listing_title_link')
+	    print (title.text)
+	    phone = i.find('a', class_='link_listing_number')
+	    print ('Phone:', phone.text)
+	    address = i.find('div', class_='listing_address')
+	    print ('Address:', address.text)
+	
